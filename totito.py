fcc1dc9a185ba786beabd79f9533593907e31b8a
ericka1599 <erickasegura96@gmail.com>
2016-07-11 08:47:04 -0600

    INTENTO 312E58

--- totito.py
+++ totito.py
@@ -1,6 +1,6 @@
-tab = [[A, B, C]
-[D, E, F]
-[G, H, I]]
+tab1 = ["A" , "B" , "C"]
+tab2 = ["D" , "E" , "F"]
+tab3 = ["G" , "H" , "I"]
 
 cont = 0
 turn = 1
@@ -23,33 +23,36 @@ while gan == False:
 		mar = "O"
 		cont = cont - 1
 
-	print (tab)
-	mov = input(jug + "Donde deseas marcar? ")
-	if mov == A:
-		tab[0,0] = mar
-	elif mov == B:
-		tab[0,1] = mar
-	elif mov == C:
-		tab[0,2] == mar
-	if mov == D:
-		tab[1,0] = mar
-	elif mov == E:
-		tab[1,1] = mar
-	elif mov == F:
-		tab[1,2] == mar
-	if mov == G:
-		tab[2,0] = mar
-	elif mov == H:
-		tab[2,1] = mar
-	elif mov == I:
-		tab[2,2] == mar
+	print (tab1)
+	print (tab2)
+	print (tab3)
+
+	mov = input(jug + " donde deseas marcar? ")
+	if mov == "A":
+		tab1[0] = mar
+	elif mov == "B":
+		tab1[1] = mar
+	elif mov == "C":
+		tab1[2] == mar
+	if mov == "D":
+		tab2[0] = mar
+	elif mov == "E":
+		tab2[1] = mar
+	elif mov == "F":
+		tab2[2] == mar
+	if mov == "G":
+		tab3[0] = mar
+	elif mov == "H":
+		tab3[1] = mar
+	elif mov == "I":
+		tab3[2] == mar
 	turn = turn + 1
 	 
 
-	if tab[0,0] and tab[0,1] and tab[0,2] == "X" or tab[1,0] and tab[1,1] and tab[1,2] == "X" or tab[2,0] and tab[2,1] and tab[2,2] == "X" or tab[0,0] and tab[1,0] and tab[2,0] == "X" or tab[0,1] and tab[1,1] and tab[2,1] == "X" or tab[0,2] and tab[1,2] and tab[2,2] == "X" or tab[0,0] and tab[1,1] and tab[2,2] == "X" or tab[0,2] and tab[1,1] and tab[2,0] == "X" :
+	if tab1[0] and tab1[1] and tab1[2] == "X" or tab2[0] and tab2[1] and tab2[2] == "X" or tab3[0] and tab3[1] and tab3[2] == "X" or tab1[0] and tab2[0] and tab3[0] == "X" or tab1[1] and tab2[1] and tab3[1] == "X" or tab1[2] and tab2[2] and tab3[2] == "X" or tab1[0] and tab2[1] and tab3[2] == "X" or tab1[2] and tab2[1] and tab3[0] == "X" :
 		gan = True
 		print ("Ganador: " + jug1 )
-	elif tab[0,0] and tab[0,1] and tab[0,2] == "O" or tab[1,0] and tab[1,1] and tab[1,2] == "O" or tab[2,0] and tab[2,1] and tab[2,2] == "O" or tab[0,0] and tab[1,0] and tab[2,0] == "O" or tab[0,1] and tab[1,1] and tab[2,1] == "O" or tab[0,2] and tab[1,2] and tab[2,2] == "O" or tab[0,0] and tab[1,1] and tab[2,2] == "O" or tab[0,2] and tab[1,1] and tab[2,0] == "O" :
+	elif tab1[0] and tab1[1] and tab1[2] == "O" or tab2[0] and tab2[1] and tab2[2] == "O" or tab3[0] and tab3[1] and tab3[2] == "O" or tab1[0] and tab2[0] and tab3[0] == "O" or tab1[1] and tab2[1] and tab3[1] == "O" or tab1[2] and tab2[2] and tab3[2] == "O" or tab1[0] and tab2[1] and tab3[2] == "O" or tab1[2] and tab2[1] and tab3[0] == "O" :
 		gan = True
 		print ("Ganador: " + jug2 )
 
@@ -57,6 +60,6 @@ while gan == False:
 
 		print ("Turno #" + turn )
 
-	if turn == 9:
+	if turn == 10:
 		gan = True
 		print ("Nadie gana")
