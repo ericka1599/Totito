c01756fbd5c49cfb33dd91893764aca18c573620
ericka1599 <erickasegura96@gmail.com>
2016-07-12 08:46:16 -0600

    casi funciona

---
+++ prueba.py
@@ -0,0 +1,6 @@
+tab = [["A" , "B" , "C"]
+["D" , "E" , "F"]
+["G" , "H" , "I"]]
+
+jug1 = input("Ingrese el nombre del primer jugador: ")
+jug2 = input("Ingrese el nombre del segundo jugador: ")
--- totito.py
+++ totito.py
@@ -1,6 +1,6 @@
-tab1 = ["A" , "B" , "C"]
-tab2 = ["D" , "E" , "F"]
-tab3 = ["G" , "H" , "I"]
+tab = [["A" , "B" , "C"]
+["D" , "E" , "F"]
+["G" , "H" , "I"]]
 
 cont = 0
 turn = 1
@@ -23,42 +23,28 @@ while gan == False:
 		mar = "O"
 		cont = cont - 1
 
-	print (tab1)
-	print (tab2)
-	print (tab3)
+	print ("Turno #" + turn)
+	turn = turn + 1
+
+	print (tab[0])
+	print (tab[1])
+	print (tab[2])
 
 	mov = input(jug + " donde deseas marcar? ")
-	if mov == "A":
-		tab1[0] = mar
-	elif mov == "B":
-		tab1[1] = mar
-	elif mov == "C":
-		tab1[2] == mar
-	if mov == "D":
-		tab2[0] = mar
-	elif mov == "E":
-		tab2[1] = mar
-	elif mov == "F":
-		tab2[2] == mar
-	if mov == "G":
-		tab3[0] = mar
-	elif mov == "H":
-		tab3[1] = mar
-	elif mov == "I":
-		tab3[2] == mar
-	turn = turn + 1
-	 
 
-	if tab1[0] and tab1[1] and tab1[2] == "X" or tab2[0] and tab2[1] and tab2[2] == "X" or tab3[0] and tab3[1] and tab3[2] == "X" or tab1[0] and tab2[0] and tab3[0] == "X" or tab1[1] and tab2[1] and tab3[1] == "X" or tab1[2] and tab2[2] and tab3[2] == "X" or tab1[0] and tab2[1] and tab3[2] == "X" or tab1[2] and tab2[1] and tab3[0] == "X" :
+	mov_valor = ord(mov) - 65
+	tiro_fila = tiro_valor % 3
+	tiro_columna = tiro_valor // 3
+
+	tab [tiro_fila] [tiro_columna] = mar
+
+	if tab [0] == 3
 		gan = True
 		print ("Ganador: " + jug1 )
-	elif tab1[0] and tab1[1] and tab1[2] == "O" or tab2[0] and tab2[1] and tab2[2] == "O" or tab3[0] and tab3[1] and tab3[2] == "O" or tab1[0] and tab2[0] and tab3[0] == "O" or tab1[1] and tab2[1] and tab3[1] == "O" or tab1[2] and tab2[2] and tab3[2] == "O" or tab1[0] and tab2[1] and tab3[2] == "O" or tab1[2] and tab2[1] and tab3[0] == "O" :
+	
 		gan = True
 		print ("Ganador: " + jug2 )
 
-	else :
-
-		print ("Turno #" + turn )
 
 	if turn == 10:
 		gan = True
